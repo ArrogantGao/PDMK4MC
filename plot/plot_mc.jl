4affe36d19035f80c0ddcfc488b1e6344e19a630
xzgao <xz.gao@connect.ust.hk>
2025-07-27 17:21:32 +0800

    update figs

--- plot/plot_mc.jl
+++ plot/plot_mc.jl
@@ -7,13 +7,13 @@ df_runtime = CSV.read("data/mc_runtime.csv", DataFrame)
 df_ewald_runtime = CSV.read("data/ewald_runtime.csv", DataFrame)
 
 begin
-    fig = Figure(size = (1000, 400), fontsize = 16)
+    fig = Figure(size = (1000, 400), fontsize = 20)
     ax_1 = Axis(fig[1, 1], xlabel = "N", ylabel = "absolute error", xscale = log10, yscale = log10)
     ax_2 = Axis(fig[1, 2], xlabel = "N", ylabel = "time (s)", xscale = log10)
 
 
     scatter!(ax_1, df_accuracy.n_src, df_accuracy.abserr_dmk, markersize = 12, marker = :diamond, color = :blue, label = "DMK")
-    scatter!(ax_1, df_accuracy.n_src, df_accuracy.abserr_ewald, markersize = 12, marker = :triangle, color = :red, label = "Ewald")
+    scatter!(ax_1, df_accuracy.n_src, df_accuracy.abserr_ewald, markersize = 12, marker = :utriangle, color = :red, label = "Ewald")
 
     # xlims!(ax_1, 10^(3.8), 10^(5.2))
     ylims!(ax_1, 1e-5, 1e-1)
@@ -33,13 +33,13 @@ begin
     scatter!(ax_2, df_runtime.n_src, df_runtime.time_total, markersize = 12, marker = :diamond, color = :blue)
     lines!(ax_2, ns, model(log10.(ns), p), color = :blue, linewidth = 2, linestyle = :dash)
 
-    scatter!(ax_2, df_ewald_runtime.n_src, df_ewald_runtime.time_total, markersize = 12, marker = :triangle, color = :red)
+    scatter!(ax_2, df_ewald_runtime.n_src, df_ewald_runtime.time_total, markersize = 12, marker = :utriangle, color = :red)
     lines!(ax_2, ns, 10.0 .^ model_ewald(log10.(ns), p_ewald), color = :red, linewidth = 2, linestyle = :dash)
 
     axislegend(ax_1, position = :rb)
 
     xlims!(ax_2, 10^(3.8), 10^(8.2))
-    ylims!(ax_2, 0, 0.03)
+    ylims!(ax_2, -0.01, 0.09)
 
     save("figs/mc_accuracy.pdf", fig)
 
